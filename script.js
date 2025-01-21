55f3543a6b130e765ec5f20948f569c60a290d9b
LeonWinde <leon.winde@dci-student.org>
2025-01-21 15:31:32 +0100

    Alles zusammengefuegt

--- script.js
+++ script.js
@@ -3,20 +3,42 @@ const rightButton = document.getElementById("right");
 const paragraph = document.getElementById("paragraph");
 const titel = document.getElementById("titel");
 const image = document.getElementById("image");
+
 const information = [
   {
-    titel: "Wale",
+    titel: "Buckelwal",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi_ZID5maG5NwNY7MlQojiunQKhZTdkkBj2-D0fl5xwFrQUNXCYjrN5qmOwORNY5SKlzf8QK6fh6r2gUn5xWQ_SQ",
+      "https://images.pexels.com/photos/4666753/pexels-photo-4666753.jpeg?auto=compress&cs=tinysrgb&w=600",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, corporis?",
+      "Der Buckelwal (Megaptera novaeangliae) ist ein oft in Küstennähe vorkommender Vertreter der Furchenwale. Er erreicht eine Körpergröße von 12 bis 15 Metern und hat im Vergleich zu anderen Walen deutlich größere Flipper. Bekannt sind die Tiere unter anderem aufgrund ihres Walgesangs und ihrer Lebhaftigkeit. Durch die intensive Bejagung gingen die weltweiten Bestände zeitweise bedrohlich zurück. Seit 1966 steht der Buckelwal unter weltweitem Artenschutz.",
   },
   {
-    titel: "Wale2",
+    titel: "Blauwal",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/e/e2/Southern_right_whale.jpg",
+      "https://media.gettyimages.com/id/639204140/de/foto/blue-whale-balaenoptera-musculus-gulf-of-california-mexico.jpg?s=612x612&w=0&k=20&c=UZyLFugHbO4lQU5BXpXOdaTU5OzwFCYpoL7X8EVXxRY=",
     paragraph:
-      "Whales are fully aquatic, open-ocean animals: they can feed, mate, give birth, suckle and raise their young at sea.",
+      "Der Blauwal (Balaenoptera musculus) ist ein Bartenwal in der Familie der Furchenwale (Balaenopteridae). Mit einer Körperlänge von bis zu 33 Metern und einer Körpermasse von bis zu 200 Tonnen ist der Blauwal eines der größten und schwersten bekannten Tiere der Erdgeschichte. Als Kosmopolit ist diese Art in allen Ozeanen der Erde verbreitet.",
+  },
+  {
+    titel: "Orca",
+    image:
+      "https://media.gettyimages.com/id/89874480/de/foto/orca-jumping-out-of-water.jpg?s=612x612&w=0&k=20&c=BegxKEifaYS0v7H5fTFnAQ2gHznS-ATMu6mXLRpWL9M=",
+    paragraph:
+      "Der (Große) Schwertwal (Orcinus orca), auch Orca, Killerwal, Mörderwal oder veraltet Butzkopf[1] genannt, ist die größte heute vorkommende Art der Delfine. Schwertwale leben in allen Ozeanen, hauptsächlich in den Polargebieten.",
+  },
+  {
+    titel: "Delphin",
+    image:
+      "https://images.pexels.com/photos/7316200/pexels-photo-7316200.jpeg?auto=compress&cs=tinysrgb&w=600",
+    paragraph:
+      "Die Delfine oder Delphine (Delphinidae) gehören zu den Zahnwalen (Odontoceti) und sind somit Säugetiere (Mammalia), die im Wasser leben (Meeressäuger). Delfine sind die vielfältigste und mit fast 40 Arten größte Familie der Wale (Cetacea). Sie sind in allen Meeren verbreitet, einige Arten kommen auch in Flüssen vor.",
+  },
+  {
+    titel: "Gefahr für Wale",
+    image:
+      "https://media.istockphoto.com/id/1385202272/de/foto/fort-lauderdale-florida-29-februar-2016-sch%C3%B6ne-aussicht-auf-stadtkan%C3%A4le-mit-booten-und.jpg?s=2048x2048&w=is&k=20&c=BefA2zpP0L_F6OsfBDdagOVpOgkfeVPhmawl8lgohGg=",
+    paragraph:
+      "Wale sind eine der faszinierendsten und größten Tierarten der Welt. Dennoch sind sie durch verschiedene Faktoren stark gefährdet.",
   },
 ];
 currentIndex = 0;
@@ -43,3 +65,5 @@ function previousSlide() {
   image.src = information[currentIndex].image;
   paragraph.innerText = information[currentIndex].paragraph;
 }
+
+console.log(fetch("./Gefahr.html"));
